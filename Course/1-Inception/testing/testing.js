4e06c28b7beee4f7c4b6a5ad5e9949af704e5a01
Jellybeanci <gokselkucuksahin@gmail.com>
2020-11-21 06:40:35 +0300

    🐹🌩

--- Course/1-Inception/testing/testing.js
+++ Course/1-Inception/testing/testing.js
@@ -1,5 +1,10 @@
+/*
 console.log("Hi?");
 array = ["A", "B", "C", "D"];
 array.forEach((element, index) => {
   console.log("Index:", index, "Element:", element);
 });
+*/
+
+let pokemon = ["Arbok", "Raichu", "Sandshrew"];
+console.log(pokemon.join(' : '));
