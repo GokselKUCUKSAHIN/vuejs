03728a0aae7648c780bee8dbc8aedcfcab29e134
jellybeanci <gokselkucuksahin@gmail.com>
2020-11-21 10:11:16 +0300

    ✅

---
+++ Course/0-Testing-Folder-CTE/Async-Await/async-await.js
@@ -0,0 +1,24 @@
+let todoListEl = document.getElementById("myList");
+
+async function fetchPost() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  ret = "";
+  await data.forEach((item, index) => {
+    // console.log(index, item);
+    ret += `
+    <li>
+    <h4>${item.userId} <i style="color: red;">${item.id}</i></h4>
+    <p>${item.title}</p>
+    </li>
+    `;
+  });
+  return ret;
+}
+
+async function loadHTML()
+{
+  todoListEl.innerHTML = await fetchPost();
+}
+
+loadHTML();
