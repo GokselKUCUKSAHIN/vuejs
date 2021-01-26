90cc11b1252060593733e7ffc36c8bbac2f9bbcc
Jellybeanci <gokselkucuksahin@gmail.com>
2021-01-26 22:16:15 +0300

    test

--- Course/0-Testing-Folder-CTE/Async-Await/async-await.js
+++ Course/0-Testing-Folder-CTE/Async-Await/async-await.js
@@ -1,15 +1,15 @@
 let todoListEl = document.getElementById("myList");
 
 async function fetchPost() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const response = await fetch('https://opendata.ecdc.europa.eu/covid19/casedistribution/json');
   const data = await response.json();
   ret = "";
   await data.forEach((item, index) => {
     console.log(index, item);
     ret += `
     <li>
-    <h4>${item.userId} <i style="color: red;">${item.id}</i></h4>
-    <p>${item.title}</p>
+      <p>${item}</p>
     </li>
     `;
   });
