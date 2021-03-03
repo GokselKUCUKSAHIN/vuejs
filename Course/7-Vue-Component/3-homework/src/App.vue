7d3771965fcc8a2013e93411a66ae6926d5dd01b
Jellybeanci <gokselkucuksahin@gmail.com>
2021-03-03 19:47:51 +0300

    Webpack Simpke

---
+++ Course/7-Vue-Component/3-homework/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <v-header></v-header>
+    <hr />
+    <v-list></v-list>
+    <hr />
+    <v-footer></v-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/common/Header";
+import List from "./components/List";
+import Footer from "./components/common/Footer";
+export default {
+  components: {
+    "v-header": Header,
+    "v-list": List,
+    "v-footer": Footer
+  }
+};
+</script>
---
+++ Course/7-Vue-Component/3-homework/src/components/ServerStatus.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <p>Sunucu Bilgisi güncel değil!!</p>
+  </div>
+</template>
+
+<style scoped>
+  div{
+    border: 2px solid yellow;
+  }
+</style>
