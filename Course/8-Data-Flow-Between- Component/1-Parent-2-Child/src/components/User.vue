bfcea7c1f3c23af19f44878c00eb680f5eaee0c6
jellybeanci <gokselkucuksahin@gmail.com>
2021-03-07 07:22:44 +0300

    Prop details

--- Course/8-Data-Flow-Between- Component/1-Parent-2-Child/src/components/User.vue
+++ Course/8-Data-Flow-Between- Component/1-Parent-2-Child/src/components/User.vue
@@ -2,13 +2,17 @@
   <div class="container">
     <h1>Parent Component (User)</h1>
     <p>Burası parent component yani her şeyin import edildiği component :)</p>
+    <button @click="changeName">Adımı Değiştir</button>
+    <button @click="title = 123">Sayı gönder</button>
+    <button @click="title = undefined">Boş gönder</button>
     <hr />
     <div class="row">
-      <app-user-detail></app-user-detail>
+      <app-user-detail :name="title"></app-user-detail>
       <app-user-edit></app-user-edit>
     </div>
   </div>
 </template>
+
 <script>
 import UserDetail from "./UserDetail";
 import UserEdit from "./UserEdit";
@@ -16,6 +20,16 @@ export default {
   components: {
     "app-user-detail": UserDetail,
     "app-user-edit": UserEdit,
+  },
+  data() {
+    return {
+      title: "Göksel",
+    };
+  },
+  methods: {
+    changeName() {
+      this.title = "KÜÇÜKŞAHİN";
+    },
   },
 };
 </script>
