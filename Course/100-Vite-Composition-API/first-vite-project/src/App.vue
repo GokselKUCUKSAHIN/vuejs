b19060db0f23ced81a12f861b70d20e521e756ab
gokselkucuksahin <gokselkucuksahin@gmail.com>
2022-04-02 12:16:37 +0300

    Vue3 Composition API

---
+++ Course/100-Vite-Composition-API/first-vite-project/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import Box from './components/Box.vue';
+</script>
+
+<template>
+  <!--  <img alt="Vue logo" src="./assets/logo.png" />-->
+  <!--  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />-->
+  <Box/>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Courier, "Courier New", monospace;
+  box-sizing: border-box;
+}
+
+body {
+  display: flex;
+  align-content: center;
+  padding-top: 30px;
+  justify-content: center;
+}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
+</style>
---
+++ Course/100-Vite-Composition-API/first-vite-project/src/components/Box.vue
@@ -0,0 +1,35 @@
+<template>
+  <button @click="hasBorder = !hasBorder">Toggle Border</button>
+  <button @click="isRed = !isRed">Toggle Red Color</button>
+  <div class="box" :class="boxClasses"></div>
+</template>
+
+<script setup lang="ts">
+import {computed, ref} from "vue";
+
+const hasBorder = ref(false);
+const isRed = ref(false);
+
+const boxClasses = computed(() => {
+  return {
+    border: hasBorder.value,
+    red: isRed.value
+  };
+})
+</script>
+
+<style scoped>
+.box {
+  height: 100px;
+  width: 100px;
+  background-color: #ccc;
+}
+
+.border {
+  border: 3px dashed #666;
+}
+
+.red {
+  background-color: crimson;
+}
+</style>
